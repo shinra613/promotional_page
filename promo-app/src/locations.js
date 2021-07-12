ee397ed4edec320bb6b7693b8968ada5e02155c0
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2021-07-13 00:09:43 +0530

    -refactor

--- promo-app/src/locations.js
+++ promo-app/src/locations.js
@@ -6,6 +6,16 @@ const PromoLocations = () => (
     <div>
         <Menu />
         <div className="location">
+            <div className="search-box">
+            <input type="text" className="" placeholder="type your location"/>
+            <input type="button"value="Search"/>
+                
+            </div>
+            
+            <div className="search-result">
+
+            </div>
+
         </div>
     </div>
 );
